9c69c8560395ad806d6128b08471c2b4ef611de2
jobtnbd17 <jobtnbd@gmail.com>
2026-07-07 23:15:23 +0700

    Fixed Cor

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,14 @@ import carRouter from "./routes/car.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  }),
+);
 app.use(express.json());
 
-app.use("/api/cars",carRouter)
+app.use("/api/cars", carRouter);
 
 export default app;
